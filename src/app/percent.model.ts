579db0ff8dad33c0092973abed246d2580596072
jspruyt <jonathan.spruyt@gmail.com>
2017-09-24 20:30:16 +0200

    percent template

--- src/app/percent.model.ts
+++ src/app/percent.model.ts
@@ -1,13 +1,55 @@
-export class Fragment {
+interface IFragment {
+
+    render(): string;
 
 }
 
-export class TextFragment extends Fragment {
+export class Pipe {
 
-    
+    constructor(
+        private pipe: string,
+        private parameter: string
+    ) {}
+
+    public apply(input) {
+        return this[this.pipe](input, this.parameter);
+    }
+
+    private upperCase(input: string, parameter: string) {
+        return input.toUpperCase();
+    }
+
+
 }
 
-export class VariableFragment extends Fragment {
+
+export class TextFragment implements IFragment {
+    constructor (
+        private text: string,
+    ) {}
+
+    public render() {
+        return this.text;
+    }
+
+}
+
+export class VariableFragment implements IFragment {
+
+
+    constructor (
+        private variable: string,
+        private value: string,
+        private pipes: Pipe[]
+    ) {}
+
+    public render() {
+        return '';
+    }
+
+    public addPipe(pipe: Pipe) {
+        this.pipes.push(pipe);
+    }
 
 }
 
